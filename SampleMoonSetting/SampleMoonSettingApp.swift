dc2decbee6b8216a415d84b50eaac133983a1d3f
MoonGoon <moongoon.cnu@gmail.com>
2023-01-27 14:51:45 +0900

    Initial Commit

---
+++ SampleMoonSetting/SampleMoonSettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMoonSettingApp.swift
+//  SampleMoonSetting
+//
+//  Created by cnu on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct SampleMoonSettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
